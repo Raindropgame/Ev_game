95db89008a1a9d80e71d2bb661de966323654b0d
Raindrop1319 <admin@raindropgame.cn>
2018-05-07 00:01:27 +0800

    12th update , 90%完成forest-2场景

---
+++ Assets/Script/HaloMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HaloMove : MonoBehaviour {
+
+    //  环形物运动
+
+    public float speed = 0.3f;
+    public Material material;
+	
+
+	void Update () {
+        material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
+        if(material.mainTextureOffset.x >= 1000)
+        {
+            material.mainTextureOffset = Vector2.zero;
+        }
+	}
+}
